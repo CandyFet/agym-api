adc5fe37b27243b6a45a8ed3adfe0f230c2c7d78
CandyFet <inorect.a.z@gmail.com>
2020-05-21 17:34:39 +0300

    Added articles, likes and reposts to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,24 @@ Post.count.times do |post_index|
         )
     end
 end
+
+25.times do |n|
+    article = Article.create(title: "Post title #{n}",
+                text: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
+                user: User.find_by(id: "#{Random.new.rand(1...User.count)}"))
+    like = Like.create(user: article.user, likeble: article)
+    repost = Repost.create(user: article.user, repostable: article)
+end
+
+Article.count.times do |article_index|
+    10.times do
+        article = Article.find_by(id: "#{article_index + 1}")
+        user = User.find_by(id: "#{Random.new.rand(1...User.count)}")
+        comment = Comment.create(
+            commentable: article,
+            user: user,
+            text: Faker::Lorem.paragraph_by_chars(number: 100, supplemental: false),
+        )
+        like = Like.create(user: user, likeble: comment)
+    end
+end
